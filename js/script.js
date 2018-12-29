c5243e891cfd75940676fd6952aed2b7bbca8ae0
YevheniiVdovin <32207802+YevheniiVdovin@users.noreply.github.com>
2018-12-29 10:18:09 +0100

    add
    new

---
+++ js/script.js
@@ -0,0 +1,9 @@
+menu.onclick = function myFunction() {
+	var x = document.getElementById("mynavigation");
+
+	if(x.className === "navigation") {
+		x.className += " responsive";
+	} else {
+		x.className = "navigation";
+	}
+}
